afbaf70dd43da994ad80627172e4ffbcbea0d275
TuningLemon <veststar1@gmail.com>
2020-07-21 22:56:44 +0900

    using map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt="pic" />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name: "Tesla",
+    image:
+      "https://image.chosun.com/sitedata/image/202007/19/2020071900291_0.jpg",
+  },
+  {
+    name: "Amazon",
+    image:
+      "https://i.insider.com/5d51c9257e337f074d2d2460?width=1100&format=jpeg&auto=webp",
+  },
+  {
+    name: "Apple",
+    image:
+      "https://lh3.googleusercontent.com/proxy/JhjgI7GdJ8tusPQHp996O7EnymQgJH3Q3HLInOc2YpMS8BMb7qY76rof7lbYiq3C5gpsPCcMMAhhoIS8LxEBDqSaoJ7eTHMrNqTslwKGSeVKseIBE8hzEw",
+  },
+  {
+    name: "Google",
+    image:
+      "https://s.france24.com/media/display/6aaac352-163c-11ea-9c6b-005056bff430/w:1280/p:16x9/ALPHABET-GOOGLE.webp",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map((comp) => (
+        <Food name={comp.name} picture={comp.image} />
+      ))}
     </div>
   );
 }
