1bfd720631a45132117815f967e3774509b4050e
TuningLemon <veststar1@gmail.com>
2020-07-22 18:48:40 +0900

    timeout componentDidMount

--- src/App.js
+++ src/App.js
@@ -3,24 +3,15 @@ import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0,
-  };
-  add = () => {
-    // this.setState({ count: this.state.count + 1 });
-    this.setState((current) => ({
-      count: current.count + 1,
-    }));
-  };
-
-  minus = () => {
-    // this.setState({ count: this.state.count - 1 }); setState에서  state.count를 부르는 것은 좋지 않음.
-    this.setState((current) => ({
-      count: current.count - 1,
-    }));
+    isLoading: true,
+    movies: [],
   };
 
   componentDidMount() {
     console.log("Component rendered");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
   componentDidUpdate() {
     console.log("I just updated");
@@ -30,14 +21,8 @@ class App extends React.Component {
   }
 
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1> The number is: {this.state.count} </h1>
-        <button onClick={this.add}>add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
