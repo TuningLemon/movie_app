1aa4dc4d6b1828ce40d4af50c7f011b6c8fa71fd
TuningLemon <veststar1@gmail.com>
2020-07-22 18:39:16 +0900

    Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,67 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Company({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/10.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    // this.setState({ count: this.state.count + 1 });
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
+  };
 
-const companyILike = [
-  {
-    id: 1,
-    rating: 4,
-    name: "Tesla",
-    image:
-      "https://image.chosun.com/sitedata/image/202007/19/2020071900291_0.jpg",
-  },
-  {
-    id: 2,
-    rating: 5,
-    name: "Amazon",
-    image:
-      "https://i.insider.com/5d51c9257e337f074d2d2460?width=1100&format=jpeg&auto=webp",
-  },
-  {
-    id: 3,
-    rating: 6,
-    name: "Apple",
-    image:
-      "https://lh3.googleusercontent.com/proxy/JhjgI7GdJ8tusPQHp996O7EnymQgJH3Q3HLInOc2YpMS8BMb7qY76rof7lbYiq3C5gpsPCcMMAhhoIS8LxEBDqSaoJ7eTHMrNqTslwKGSeVKseIBE8hzEw",
-  },
-  {
-    id: 4,
-    rating: 7,
-    name: "Google",
-    image:
-      "https://s.france24.com/media/display/6aaac352-163c-11ea-9c6b-005056bff430/w:1280/p:16x9/ALPHABET-GOOGLE.webp",
-  },
-];
+  minus = () => {
+    // this.setState({ count: this.state.count - 1 }); setState에서  state.count를 부르는 것은 좋지 않음.
+    this.setState((current) => ({
+      count: current.count - 1,
+    }));
+  };
 
-Company.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
+  componentDidMount() {
+    console.log("Component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+  }
 
-function renderCompany(comp) {
-  console.log(comp);
-  return (
-    <Company
-      key={comp.id}
-      rating={comp.rating}
-      name={comp.name}
-      picture={comp.image}
-    />
-  );
-}
-
-function App() {
-  return <div>{companyILike.map(renderCompany)}</div>;
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1> The number is: {this.state.count} </h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
